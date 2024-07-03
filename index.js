ecf811c2853d1f42927f90d726befb729b498696
princewillduru <duruprincewilluzochukwu@gmail.com>
2024-07-03 09:38:13 +0100

    add req for debuging

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("api/enrollment", async (req, res) => {
+app.post("/api/enrollment", async (req, res) => {
   const token = req.body["g-recaptcha-response"];
   const remoteIp = req.connection.remoteAddress;
 console.log("req",req);
